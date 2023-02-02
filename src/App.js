27ea3fe13c5eaebed36451fa6ea8090b082ac07a
andriideikalo <wlada801@gmail.com>
2023-02-02 22:55:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+// const App = () => {
+//   const [value, setValue] = useState(0);
+
+//   return (
+//     <div>
+//       {value}
+//       <button type="button" onClick={() => setValue(value + 1)}>
+//         Increment value by 1
+//       </button>
+//     </div>
+//   );
+// };
+
+// ________________________________________________
+
+import { useState, useEffect } from "react";
+
+// const App = () => {
+//   const [value, setValue] = useState(0);
+
+//   useEffect(() => {
+//     document.title = `You clicked ${value} times`;
+//   });
+
+//   return (
+//     <div>
+//       <p>You clicked {value} times</p>
+//       <button onClick={() => setValue(value + 1)}>Click me</button>
+//     </div>
+//   );
+// };
+
+// __________________________________________________
 
 const App = () => {
   const [value, setValue] = useState(0);
 
-  return (
-    <div>
-      {value}
-      <button type="button" onClick={() => setValue(value + 1)}>
-        Increment value by 1
-      </button>
-    </div>
-  );
+  useEffect(() => {
+    console.log("Mouting phase: same when componentDidMount runs");
+  }, [value]);
+
+  return <button onClick={() => setValue(value + 1)}>{value}</button>;
 };
 
 export default App;
